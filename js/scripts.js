40a67544c1021a43aa5c643a420209d9004f7f41
toddmac12 <toddmac12@gmail.com>
2021-08-10 18:20:49 -0400

    add updated changes to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,79 @@
 $(document).ready(function() {
-    $(".clickable").click(function() {
-        $("#walrus-showing").toggle();
-        $("#walrus-hidden").toggle();
+    $("#header1").click(function() {
+        $("#p1").toggle();
     });
+
+    $("#header2").click(function() {
+        $("#p2").toggle();
+    });
+
+    $("#header3").click(function() {
+        $("#p3").toggle();
+    });
+
+    $("#header4").click(function() {
+        $("#p4").toggle();
+    });
+
+    $("#header5").click(function() {
+        $("#p5").toggle();
+    });
+
+    $("#header6").click(function() {
+        $("#p6").toggle();
+    });
+
+    $("#header7").click(function() {
+        $("#p7").toggle();
+    });
+
+    $("#header8").click(function() {
+        $("#p8").toggle();
+    });
+
+    $("#header9").click(function() {
+        $("#p9").toggle();
+    });
+
+    $("#header10").click(function() {
+        $("#p10").toggle();
+    });
+
+    $("#header11").click(function() {
+        $("#p11").toggle();
+    });
+
+    $("#header12").click(function() {
+        $("#p12").toggle();
+    });
+
+    $("#header13").click(function() {
+        $("#p13").toggle();
+    });
+
+    $("#header14").click(function() {
+        $("#p14").toggle();
+    });
+
+    $("#header15").click(function() {
+        $("#p15").toggle();
+    });
+
+    $("#header16").click(function() {
+        $("#p16").toggle();
+    });
+
+    $("#header17").click(function() {
+        $("#p17").toggle();
+    });
+
+    $("#header18").click(function() {
+        $("#p18").toggle();
+    });
+
+    $("#header19").click(function() {
+        $("#p19").toggle();
+    });
+
+
 });
